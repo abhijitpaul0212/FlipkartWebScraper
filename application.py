605b05d6166dc802220b5828868328e91691b75a
Abhijit Paul <abhijitpaul0212@gmail.com>
2023-08-04 23:53:12 +0530

    Added number of browser pages to search from

--- application.py
+++ application.py
@@ -25,11 +25,11 @@ class WebScrape:
         self.reviews = []        
 
     def store_in_df(self):
-        print(len(self.products), len(self.ratings), len(self.prices), len(self.reviews), len(self.specifications))
+        # print(len(self.products), len(self.ratings), len(self.prices), len(self.reviews), len(self.specifications))
         if all([len(self.products), len(self.ratings), len(self.prices), len(self.reviews), len(self.specifications)]):
-            df = pd.DataFrame({'Product Name': self.products, 'Overall Ratings': self.ratings, 'Price': self.prices, 'Specification': self.specifications, 'Reviews': self.reviews})
-            # print(df.head(10))
-            return df
+            df = pd.DataFrame({'Product Name': self.products, 'Overall Rating': self.ratings, 'Price': self.prices, 'Specification': self.specifications, 'Customer Reviews': self.reviews})
+            # print(df.head(2))
+            return df 
         else:
             print("Cannot store data since one/all column(s) has zero values scrapped")
     
@@ -39,18 +39,18 @@ class WebScrape:
         flipkart_coll.delete_many({}) 
         
         df = self.store_in_df()
-        print(df)
+        # print(df)
         if df is not None:
             df.reset_index(inplace=True)
             data_dict = df.to_dict("records")
             flipkart_coll.insert_many(data_dict) 
 
 
-    def web_scrape(self):
+    def web_scrape(self, number_of_pages: int):
         base_url = "https://www.flipkart.com"
         query_url = f"/search?q={self.query}&as=on&as-show=on&otracker=AS_Query_TrendingAutoSuggest_8_0_na_na_na&otracker1=AS_Query_TrendingAutoSuggest_8_0_na_na_na&as-pos=8&as-type=TRENDING&suggestionId=tv&requestId=9c9fa553-b7e5-454b-a65b-bbb7a9c74a29&page=1"
         final_url = base_url + query_url
-        for _ in range(10):
+        for _ in range(number_of_pages):
             response = requests.get(final_url)
             if response.status_code != 200:
                 break
@@ -67,6 +67,7 @@ class WebScrape:
                 
                 rating = row.find('div', attrs={'class': '_3LWZlK'})
                 self.ratings.append(rating.text if rating is not None else 'NA')
+                # print(self.ratings)
                 
                 all_spec = row.find('div', attrs={'class': 'fMghEO'})
                 spec = all_spec.find_all('li', class_ = 'rgWa7D')
@@ -107,18 +108,19 @@ class WebScrape:
 def home():
     if request.method == "POST":
         search_text = request.form.get("search")
+        num_pages = request.form.get("num_pages")
+
         ws = WebScrape(query=search_text)
-        ws.web_scrape()
+        ws.web_scrape(number_of_pages=int(num_pages))
         ws.store_in_db()
         return redirect(url_for('search_results'))
     return render_template("home.html", text="None")
 
 @app.route("/search_results")
 def search_results():
-    headings = ('Product Name', 'Customer Ratings', 'Price', 'Specification', 'Reviews')
+    headings = ('Product Name', 'Overall Rating', 'Price', 'Specification', 'Customer Reviews')
     results = []
     for result in flipkart_coll.find({}):
-        print(result.get(headings[4]))
         results.append((result.get(headings[0]), result.get(headings[1]), result.get(headings[2]), result.get(headings[3]), result.get(headings[4])))
     if results == []:
         flash("No data found... try again!")
